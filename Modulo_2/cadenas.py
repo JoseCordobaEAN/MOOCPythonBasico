2e2fb3dfaa6b27905f35da0282a04b20d7e7d6d9
Jose Cordoba <jmcordoba@universidadean.edu.co>
2019-03-08 09:00:46 -0500

    Agregados ejercicios de números y de cadenas

---
+++ Modulo_2/cadenas.py
@@ -0,0 +1,29 @@
+cadena = 'esta es mi cadena'
+# print(cadena[0])
+# print(cadena[3])
+# print(cadena[-1])
+
+# print(cadena[5:7])
+
+# print(cadena[12:])
+# print(cadena[-5: -1])
+
+# print(cadena.capitalize())
+# print(cadena.count('a'))
+# print(cadena.endswith('cadena'))
+
+print(cadena.find('mi'))
+
+nombre = 'jose'
+apellido = 'cordoba'
+
+saludo = 'Tu apellido es {1} y tu nombre es {0}'.format(nombre, apellido)
+print(saludo)
+
+print(apellido.isalpha())
+print(apellido.isdecimal())
+print(apellido.isalnum())
+
+cadena = "     cadena           "
+print(cadena)
+print(cadena.strip())
---
+++ Modulo_2/numeros.py
@@ -0,0 +1,12 @@
+numero_1 = input('Ingrese su primer numero')
+numero_1 = float(numero_1)
+numero_2 = input('Ingrese su segundo numero')
+numero_2 = float(numero_2)
+numero_3 = input('Ingrese su tercer numero')
+numero_3 = float(numero_3)
+
+
+print('resultado de la suma de sus numeros es', numero_1 + numero_2 + numero_3)
+print('resultado de la suma de sus numeros es', numero_1 - numero_2 - numero_3)
+print('resultado de la suma de sus numeros es', numero_1 * numero_2 * numero_3)
+print('resultado de la suma de sus numeros es', numero_1 / numero_2 / numero_3)
